bebec932bfae6ceae6b03360d81a74fff85af779
andriawang6 <andria@gatech.edu>
2025-04-21 17:11:46 -0400

    update microcontroller for potentiometer input

--- software/motorcontroller.ino
+++ software/motorcontroller.ino
@@ -7,7 +7,7 @@ Servo bubbleSpin;
 const int udPin = 5;
 const int lrPin = 6;
 const int bubbleSpinPin = 7;
-// # when on, set to 100, when off, set to 90 
+const int bubblePotPin = A0; 
 
 int udPos = 90; // Start at middle
 int lrPos = 90;
@@ -19,9 +19,18 @@ const int updateDelay = 15;    // ms between servo updates
 
 unsigned long lastUpdate = 0;
 
+int bubbleSpinOn;
+int bubbleSpinSpeed;
+int change = 0;
+int prevPot = 0;
+long time = 0;
+
 void setup() {
+  pinMode(bubblePotPin, INPUT);
   bubbleSpin.attach(bubbleSpinPin);
   bubbleSpin.writeMicroseconds(1500);
+  bubbleSpinOn = 0;
+  time = millis(); 
 
   upDown.attach(udPin);
   leftRight.attach(lrPin);
@@ -36,9 +45,6 @@ void setup() {
 void loop() {
   if (Serial1.available()) {
     char command = Serial1.read();
-    Serial.print("Received: ");
-    Serial.println(command);
-
     switch (command) {
       case 'r': targetLr = constrain(targetLr - 10, 0, 180); break;
       case 'l': targetLr = constrain(targetLr + 10, 0, 180); break;
@@ -48,11 +54,17 @@ void loop() {
         targetUd = 90; 
         targetLr = 90; 
         break;
-      case 'x': bubbleSpin.writeMicroseconds(1500); break; // don't spin
-      case 's': bubbleSpin.write(100); break; // spin
+      case 'x': bubbleSpinOn = 0; break; 
+      case 's': bubbleSpinOn = 1; break; 
     }
   }
 
+  long temp = millis();
+  if (temp - time > 100) {
+    toggleBubbleSpeed();
+    time = temp;
+  }
+  
   unsigned long now = millis();
   if (now - lastUpdate > updateDelay) {
     lastUpdate = now;
@@ -68,5 +80,19 @@ void loop() {
       lrPos = constrain(lrPos, 0, 180);
       leftRight.write(lrPos);
     }
+  }
+}
+
+void toggleBubbleSpeed() {
+  int potValue = analogRead(bubblePotPin);
+  if (prevPot != potValue) {
+    prevPot = potValue;
+  }
+
+  if (bubbleSpinOn == 1) { 
+    int servoSpeed = map(potValue, 0, 1023, 1500, 2000);  
+    bubbleSpin.writeMicroseconds(servoSpeed);
+  } else {
+    bubbleSpin.writeMicroseconds(1500);
   }
 }
